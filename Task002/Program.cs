a0bad0b69ea0f98abaa4f031876423d1c4dc5d36
Pavel <pmaskarev@vk.com>
2023-03-24 11:16:30 +0300

    Added Task002

---
+++ Task002/Program.cs
@@ -0,0 +1,21 @@
+﻿int a = 0;
+int b = 0;  //Переменные для чисел
+
+System.Console.WriteLine("Приветсвтую Вас в программе для нахождения большего из двух чисел.");
+System.Console.WriteLine("Введите первое число: ");
+a = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите второе число: ");
+b = int.Parse(Console.ReadLine());      //Ввод начальных чисел
+
+if ( a>b )
+{
+    System.Console.WriteLine($"Число {a} большее, а {b} меньшее.");
+}
+else if ( a<b )
+{
+    System.Console.WriteLine($"Число {b} большее, а {a} меньшее.");
+}
+else
+{
+    System.Console.WriteLine("Введённые числа равны.");
+}
