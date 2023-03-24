8205225f345e10e9749c02c9ca23e8aaa74ca12e
Pavel <pmaskarev@vk.com>
2023-03-24 13:11:06 +0300

    Added Task008

---
+++ Task008/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = 0;
+int b = 2; 
+
+System.Console.WriteLine("Приветсвтую Вас в программе для вывода ряда четных чисел.");
+System.Console.WriteLine("Введите число, до которого будет составлен ряд.");
+a = int.Parse(Console.ReadLine());
+
+if (a<2)
+{
+    System.Console.WriteLine("Данная программа предполагает, что число будет больше либо равно 2. Введите корректное число.");
+} 
+else
+{
+    while (b<=a)
+    {
+        System.Console.Write(b+" ");
+        b= b + 2;
+    }
+}
