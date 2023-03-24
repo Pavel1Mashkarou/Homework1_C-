45a0b9f940aed27386571a1421319622d5df9637
Pavel <pmaskarev@vk.com>
2023-03-24 11:34:03 +0300

    Added Task006

---
+++ Task006/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = 0;
+int ost = 0;
+
+System.Console.WriteLine("Приветсвую Вас в программе определения чётного числа.");
+System.Console.WriteLine("Введите число:");
+a = int.Parse(Console.ReadLine());
+
+ost = a % 2;
+
+if (ost == 0)
+{
+    System.Console.WriteLine($"Число {a} - чётное.");
+}
+else
+{
+    System.Console.WriteLine($"Число {a} - нечётное.");
+}
